16c1382ea34d86c3f02ce00770ed48381f639f1d
albo <albo@trustpilot.com>
2021-11-02 16:55:34 +0100

    add listeners programatically

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ const clearSelection =(event)=> {
         selectedEl.classList.remove('selected')
     }
 }
+
+const startup =()=>{
+    const elements = document.getElementsByClassName('item')
+    for(let el of elements){
+        el.addEventListener("touchstart", handleSelection, false);
+        el.addEventListener("touchout", clearSelection, false);
+        el.addEventListener("touchcancel", clearSelection, false);
+        el.addEventListener("touchmove", handleSelection, false);
+        el.addEventListener("mouseover", handleSelection, false);
+        el.addEventListener("mouseout", clearSelection, false);
+    }
+  }
+  
+document.addEventListener("DOMContentLoaded", startup);
