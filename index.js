619ddcb769cdbe844897aacf2dcd11e0e39a2ad1
albo <albo@trustpilot.com>
2021-11-02 16:40:25 +0100

    add prevent defaults

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let selectedEl = null;
 
 const handleSelection =(event)=>{
+    event.preventDefault();
     var xcoord = event.touches ? event.touches[0].pageX : event.pageX;
     var ycoord = event.touches ? event.touches[0].pageY : event.pageY;
     var targetElement = document.elementFromPoint(xcoord, ycoord);
@@ -12,6 +13,7 @@ const handleSelection =(event)=>{
 }
 
 const clearSelection =(event)=> {
+    event.preventDefault();
     if(selectedEl){
         selectedEl.classList.remove('selected')
     }
